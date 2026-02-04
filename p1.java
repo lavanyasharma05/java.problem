83969236af4c1d64aab79756b7a8da21389e7411
lavanyasharma05 <lavanyasharma1105@gmail.com>
2026-02-04 22:29:33 +0530

    Implement getMinDiff method to calculate min difference

---
+++ p1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int getMinDiff(int[] arr, int k) {
+        // code here
+        int n = arr.length;
+        if (n == 1) return 0;
+
+        Arrays.sort(arr);
+
+        int ans = arr[n - 1] - arr[0];
+
+        int smallest = arr[0] + k;
+        int largest = arr[n - 1] - k;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minH = Math.min(smallest, arr[i + 1] - k);
+            
+        
+            int maxH = Math.max(largest, arr[i] + k);
+
+           
+            if (minH < 0) continue;
+
+            
+            ans = Math.min(ans, maxH - minH);
+        }
+
+        return ans;
+    }
+}
