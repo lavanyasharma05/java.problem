0a55e0356ca9e8b10901b3cc729db6f47038b952
lavanyasharma05 <lavanyasharma1105@gmail.com>
2026-02-04 23:43:04 +0530

    Implement method to find common elements in three lists

---
+++ p7.java
@@ -0,0 +1,32 @@
+
+class Solution {
+    public List<Integer> commonElements(List<Integer> arr1, List<Integer> arr2, List<Integer> arr3) {
+        List<Integer> res = new ArrayList<>();
+        
+        int i = 0, j = 0, k = 0;
+
+        while (i < arr1.size() && j < arr2.size() && k < arr3.size()) {
+            
+            int a = arr1.get(i);
+            int b = arr2.get(j);
+            int c = arr3.get(k);
+
+            if (a == b && b == c) {
+                res.add(a);
+                
+                while (i < arr1.size() && arr1.get(i) == a) i++;
+                while (j < arr2.size() && arr2.get(j) == a) j++;
+                while (k < arr3.size() && arr3.get(k) == a) k++;
+            } 
+            else if (a < b) {
+                i++;
+            } else if (b < c) {
+                j++;
+            } else {
+                k++;
+            }
+        }
+        
+        return res;
+    }
+}
